4b3a09421c27d15da34f6e25096f646a02c2cd68
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-06-03 12:07:08 -0500

    finished assignment

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
-                       id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-                       artist VARCHAR(255) NOT NULL ,
-                       name VARCHAR(255) NOT NULL ,
-                       release_date DATE NOT NULL ,
-                       sales DECIMAL(9,2) NOT NULL ,
-                       genre VARCHAR(255) NOT NULL ,
-                       PRIMARY KEY (id)
+    id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(255) NOT NULL ,
+    name VARCHAR(255) NOT NULL ,
+    release_date DATE NOT NULL ,
+    sales DECIMAL(9,2) NOT NULL ,
+    genre VARCHAR(255) NOT NULL ,
+    PRIMARY KEY (id)
 );
