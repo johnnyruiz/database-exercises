412375e465f1404e1651dbb23a905133838f2417
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-06-04 14:03:26 -0500

    finished 2nd half of exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -7,16 +7,19 @@ AND gender = 'M';
 
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE last_name LIKE ('%e');
+WHERE last_name LIKE ('%e')
+AND last_name LIKE('e%');
 
 SELECT emp_no, first_name, hire_date
 FROM employees
 WHERE hire_date LIKE ('199%');
 
-SELECT emp_no, first_name, hire_date
+SELECT emp_no, first_name, hire_date, birth_date
 FROM employees
-WHERE hire_date LIKE ('%12-25');
+WHERE birth_date LIKE ('%12-25')
+AND hire_date LIKE ('199%');
 
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE ('%q%');
+AND last_name NOT LIKE ('%qu%');
